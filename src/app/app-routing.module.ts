ff0e0693e5b6ade55e6f37af57d4cd9540306244
Antonio Eduardo <antonioeduardo1998@gmail.com>
2021-03-20 09:59:00 -0300

    created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MostruarioComponent } from './pages/mostruario/mostruario.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'mostruario', component: MostruarioComponent },
+  { path: 'cadastro', component: CadastroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
