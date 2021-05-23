1ba20a791c6c58187d0840447e3652d9b0560632
Antonio Oliveira <antonioeduardo1998@gmail.com>
2021-05-23 15:02:19 -0300

    add real api in carros service

--- src/app/services/carros.service.ts
+++ src/app/services/carros.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,10 @@ export class CarrosService {
   constructor(private http: HttpClient) { }
 
   getCars(): Observable<any> {
-    return this.http.get('http://localhost:3333/carros')
+    return this.http.get(environment.baseURL)
+      .pipe(
+        tap(console.log)
+      )
   }
 
 }
